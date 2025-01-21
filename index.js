14006339dcedca14c9ef59084289434d6f4369e0
Mohammad90-ui <mohammadhasan707053@gmail.com>
2025-01-21 11:55:23 +0530

    assigment done

--- index.js
+++ index.js
@@ -34,17 +34,36 @@
 
 const express = require('express');
 const { resolve } = require('path');
-
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3010;
 
+app.use(bodyParser.json());
+const students = require('./data.json');
+app.post('/students/above-threshold',(req,res)=>{
+  const {threshold} = req.body;
 
+if(typeof threshold != 'number'){
+  return res.status(400).json({error:'Invalid threshold value. It must be a number. '})
+}
 
-app.use(express.static('static'));
+const filteredStudents = students.filter((student) => student.total > threshold);
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+const response = {
+  count: filteredStudents.lenght,
+  student: filteredStudents.map(({name,total}) => ({name,total})),
+
+}
+return res.status(200).json(response);
 });
+
+
+
+// app.use(express.static('static'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(resolve(__dirname, 'pages/index.html'));
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
